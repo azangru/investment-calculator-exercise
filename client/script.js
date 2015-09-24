e314330994081e49020fd4069ae47122e867af1c
Andrey Azov <andrey.azov@gmail.com>
2015-09-25 02:12:57 +0300

    Added jQuery UI slider

--- client/script.js
+++ client/script.js
@@ -1,1 +1,18 @@
-console.log('I am loaded!');
+$(function(){
+
+  var tooltip = $('<div class="tooltip">').html('1 000 $');
+  
+  $(".slider").slider({
+    range: "min",
+    min: 100,
+    max: 10000,
+    step: 100,
+    value: 1000,
+    slide: function(event, ui) { 
+      console.log(ui.value); 
+    }
+  });
+  
+  $(".ui-slider-handle").append(tooltip);
+  
+});
